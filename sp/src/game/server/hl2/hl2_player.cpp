e56734313f2ae97cb610f268b79c6712ac25a850
ashifolfi <anticream39@yahoo.com>
2023-02-13 15:27:28 -0500

    port viewroll and speed changes

--- sp/src/game/server/hl2/hl2_player.cpp
+++ sp/src/game/server/hl2/hl2_player.cpp
@@ -89,15 +89,15 @@ extern int gEvilImpulse101;
 ConVar sv_autojump( "sv_autojump", "0" );
 
 ConVar hl2_walkspeed( "hl2_walkspeed", "150" );
-ConVar hl2_normspeed( "hl2_normspeed", "190" );
-ConVar hl2_sprintspeed( "hl2_sprintspeed", "320" );
+ConVar hl2_normspeed( "hl2_normspeed", "250" );
+ConVar hl2_sprintspeed( "hl2_sprintspeed", "420" );
 
 ConVar hl2_darkness_flashlight_factor ( "hl2_darkness_flashlight_factor", "1" );
 
 #ifdef HL2MP
 	#define	HL2_WALK_SPEED 150
-	#define	HL2_NORM_SPEED 190
-	#define	HL2_SPRINT_SPEED 320
+	#define	HL2_NORM_SPEED 250
+	#define	HL2_SPRINT_SPEED 420
 #else
 	#define	HL2_WALK_SPEED hl2_walkspeed.GetFloat()
 	#define	HL2_NORM_SPEED hl2_normspeed.GetFloat()
